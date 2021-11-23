9df41678846e5e1a09f0f10369fe73447e42550e
jersonCuevas <thejer19@gmail.com>
2021-11-23 17:38:48 -0400

    realizando peticiones

--- app.js
+++ app.js
@@ -1,11 +1,63 @@
+// http
 const http = require('http');
-const server = http.createServer((req, resp)=>{
-    resp.end('Mostrando tu peticion v1.11');
-})
+
+// const server = http.createServer((req, resp)=>{
+//     resp.end('Mostrando tu peticion v1.11');
+// })
+// const port = 2000;
+// server.listen(port, ()=>{
+//     console.log('open server')
+// });
+  
+
+
+// express
+const express = require('express');
+const app = express();
 const port = 2000;
-server.listen(port, ()=>{
-    console.log('open server')
+
+
+
+// (1) solicitudes del cliente
+app.get('/', (req, resp)=>{
+    resp.send('Respondiendo a tu solicitud v1.2')
 });
+// (2)
+app.get('/inquietudes', (req, resp)=>{
+    resp.send('Atendiendo tus inquietudes')
+});
+
+
+app.listen(port, ()=>{
+    console.log('iniciando la respuesta')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
